9bfb2d784e0a9475481fa03f22fdaf8402151087
AhmedHamed-20 <anaahmedmohamed57@gmail.com>
2022-08-16 16:41:17 +0200

    add_some_web_design

--- lib/layouts/web_layout.dart
+++ lib/layouts/web_layout.dart
@@ -1,13 +1,10 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
+import 'dart:math' as math; // import this
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:la_vie/data_layer/bloc/login_cubit/login_cubit.dart';
 import 'package:la_vie/data_layer/bloc/login_cubit/login_states.dart';
 import 'package:la_vie/presentation_layer/components/components.dart';
 import 'package:la_vie/presentation_layer/constants/constants.dart';
-
 import '../presentation_layer/screen/web_screens/login_screen_web.dart';
 import '../presentation_layer/widgets/nav_bar_web.dart';
 
@@ -19,36 +16,58 @@ class LayoutScreenWeb extends StatelessWidget {
     return BlocConsumer<LoginCubit, LoginCubitStates>(
       builder: (context, state) {
         return Scaffold(
-          body: Column(
+          body: Stack(
             children: [
-              SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: 70,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: AppPadding.p20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                          flex: 1,
-                          child: Image.asset('assets/images/logo.png')),
-                      navBarWeb(navBarList),
-                      Flexible(
-                        flex: 1,
-                        child: defaultButton(
-                            onPressed: () {},
-                            buttonChild: Text(
-                              'Sign Up',
-                              style: Theme.of(context).textTheme.labelMedium,
-                            ),
-                            width: AppWidth.w100,
-                            height: AppHeight.h46),
-                      )
-                    ],
+              Positioned(
+                left: 0,
+                top: 70,
+                child: Image.asset('assets/images/web_tree.png'),
+              ),
+              Positioned(
+                right: 0,
+                bottom: 0,
+                child: Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.rotationY(math.pi),
+                  child: Image.asset(
+                    'assets/images/web_tree.png',
                   ),
                 ),
               ),
-              Expanded(child: LoginScreenWeb()),
+              Column(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: 70,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: AppPadding.p20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Flexible(
+                            flex: 1,
+                            child: Image.asset('assets/images/logo.png'),
+                          ),
+                          navBarWeb(navBarList),
+                          Flexible(
+                            flex: 1,
+                            child: defaultButton(
+                                onPressed: () {},
+                                buttonChild: Text(
+                                  'Sign Up',
+                                  style:
+                                      Theme.of(context).textTheme.labelMedium,
+                                ),
+                                width: AppWidth.w100,
+                                height: AppHeight.h46),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Expanded(child: LoginScreenWeb()),
+                ],
+              ),
             ],
           ),
         );
