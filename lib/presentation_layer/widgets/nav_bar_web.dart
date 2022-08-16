5c9b1ea6ce1528181bec1495c1354c102a90b84c
AhmedHamed-20 <anaahmedmohamed57@gmail.com>
2022-08-16 13:29:10 +0200

    web_login_nav_bar

--- lib/presentation_layer/widgets/nav_bar_web.dart
+++ lib/presentation_layer/widgets/nav_bar_web.dart
@@ -3,26 +3,19 @@ import 'dart:js';
 import 'package:flutter/widgets.dart';
 
 import 'package:flutter/material.dart';
-import 'package:la_vie/presentation_layer/constants/constants.dart';
 
 Widget navBarWeb(List<Map<String, Widget>> screens) {
   return Flexible(
-    flex: 1,
-    child: ListView.builder(
-      scrollDirection: Axis.horizontal,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: EdgeInsets.all(AppPadding.p20),
-          child: Text(
-            screens[index]
-                .keys
-                .toString()
-                .replaceAll('(', '')
-                .replaceAll(')', ''),
-          ),
-        );
-      },
-      itemCount: screens.length,
+    flex: 2,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: const [
+        Text('Home'),
+        Text('Shop'),
+        Text('Blog'),
+        Text('About'),
+        Text('Community'),
+      ],
     ),
   );
 }
